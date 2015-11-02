46b708b53fa092eda850e76fa2b689c92bc77d45
josellausas <jose@josellausas.com>
2015-11-02 09:28:45 -0600

    Initial commit. Loads and shows an image.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+/**
+ *  main.cpp
+ *
+ *  @param  None
+ *
+ *  @return App's exit code.
+ */
+
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);
+	ofRunApp(new ofApp());
+
+}
